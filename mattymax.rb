15fb51fae07cbbf94a0aa6a18461fd6c65c5dd80
Matt Swieboda <mswiebod@gmail.com>
2015-11-24 16:13:27 -0600

    added /tmp to gitigonre, wrapped sinatra in class, WIP for parallization w middleman

--- mattymax.rb
+++ mattymax.rb
@@ -7,26 +7,35 @@
 require 'sinatra'
 require 'slim'
 
-get '/' do
-  puts '/'
-  slim :root
-end
+# TODO: make sinatra work in parallel with middleman
+class MattyMax < Sinatra::Base
+  get '/' do
+    puts '/'
+    slim :root
+  end
 
-# [noun] say [verb] to [noun]
-get '/*/say/*/to/*' do
-  puts '/*/say/*/to/*'
-  third_p, shit, thing = params['captures']
-  thing.match(/mattymax/i) ? "thx, h3ll0 #{third_p}" : "I'd bet #{thing} thinks so"
-end
+  # [noun] say [verb] to [noun]
+  get '/shit' do
+    puts '/shit'
+    "fuck"
+  end
 
-# captures
-# get '/:path.?:format?' do
-get /\A\/(.*)\?*\z/, :provides => 'html' do
-  puts '/\A\/(.*)\?*\z/'
-  params['captures'].each { |c| puts c }
-  slim :index
-end
+  # [noun] say [verb] to [noun]
+  get '/*/says/*/to/*' do
+    puts '/*/says/*/to/*'
+    third_p, shit, thing = params['captures']
+    thing.match(/mattymax/i) ? "thx, h3ll0 #{third_p}" : "I'd bet #{thing} thinks so"
+  end
 
-get '/*' do
-  slim :index
+  # captures
+  # get '/:path.?:format?' do
+  get /\A\/(.*)\?*\z/, :provides => 'html' do
+    puts '/\A\/(.*)\?*\z/'
+    params['captures'].each { |c| puts c }
+    slim :index
+  end
+
+  get '/*' do
+    slim :index
+  end
 end
