a508ddac4f4ef97f4e509c112b963182b1bbdf41
Matt Swieboda <mswiebod@gmail.com>
2015-11-24 03:29:06 -0600

    adds gemfile, thin, slim, routes

--- mattymax.rb
+++ mattymax.rb
@@ -1,6 +1,32 @@
-# myapp.rb
+# /----------------------------------------------------------.
+# |  mattymax.rb                                             |
+#_|                                                          |
+# TODO: swash commits (the wierd ones, AKA most of them?)    |
+#\___________________________________________________________/
+
 require 'sinatra'
+require 'slim'
 
 get '/' do
-  'h3ll0 mattymax!'
+  puts '/'
+  'h3ll0 dude(tte)!'
+end
+
+# [noun] say [verb] to [noun]
+get '/*/say/*/to/*' do
+  puts '/*/say/*/to/*'
+  third_p, shit, thing = params['captures']
+  thing.match(/mattymax/i) ? "thx, h3ll0 #{third_p}" : "I'd bet #{thing} thinks so"
+end
+
+# captures
+# get '/:path.?:format?' do
+get /\A\/(.*)\?*\z/, :provides => 'html' do
+  puts '/\A\/(.*)\?*\z/'
+  params['captures'].each { |c| puts c }
+  slim :index
+end
+
+get '/*' do
+  slim :index
 end
